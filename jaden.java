030bd7b0d0d8127076946fc7f69c4f51f7976176
jadeyy <jadda2522@gmail.com>
2025-08-21 23:55:18 +0530

    Your commit message here

---
+++ jaden.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Do numbers ko define karte hain
+        int num1 = 10;
+        int num2 = 20;
+
+        // Addition perform karna
+        int sum = num1 + num2;
+
+        // Result print karna
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
